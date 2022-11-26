d4e4a63168c8550d1e9704e8263259cbed8a022e
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-11-26 15:32:08 +1300

    update <meta> tags and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,8 +26,22 @@ export default function Home() {
   return (
     <div>
     <Head>
-      <title>Robert Pether Project Web Application</title>
+      <title>Robert Pether: React Developer Personal Website</title>
+      {/* meta tags */}
+      <meta name="author" content="Robert Pether" />
+      <meta
+        name="description"
+        content="Robert Pether React Developer Personal Website" />
+      <meta 
+        name="keywords" content="React Next Node MongoDB Azure AWS Flask 
+        CV Robert Pether Junior Front-End Developer Personal Website "/>
+      <meta 
+        name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* links & APIs */}
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet" />
     </Head>
     <Suspense fallback={`Loading...`}>
       <Navbar />
