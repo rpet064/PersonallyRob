235a064ca68e2697a8edb710cd94ce9566df7bd3
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-02 13:54:04 +1300

    mobile responsiveness in progress

--- pages/about/me.tsx
+++ pages/about/me.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import aboutInfo from './aboutInfo';
+import aboutInfo from './aboutInfo.json';
 
 const Header = dynamic(() => import('../../components/header'), {
   suspense: true,
@@ -43,6 +43,10 @@ export default function Me() {
   const [elementClass1, setElementClass1] = useState("image-div");
   const [elementClass2, setElementClass2] = useState("image-div");
   const [elementClass3, setElementClass3] = useState("image-div");
+  const [elementClass4, setElementClass4] = useState("image-div");
+  const [elementClass5, setElementClass5] = useState("image-div");
+  const [elementClass6, setElementClass6] = useState("image-div");
+  const [elementClass7, setElementClass7] = useState("image-div");
 
 
     return (
@@ -50,7 +54,7 @@ export default function Me() {
          <Header />
           <Navbar />
             <h1 className="large-header">Robert Pether</h1>
-            <h1 className="medium-header">About</h1>
+            <h1 id="about-header" className="medium-header">About</h1>
               <div className="image-container">
                 <div className="row-1">
                     <Image
@@ -102,26 +106,25 @@ export default function Me() {
                   </div>
           </div>
           <div id="Portfolio">
-            <h1 className="medium-header">Portfolio</h1>
+            <h1 id="portfolio-header" className="medium-header">Portfolio</h1>
             <div className="image-container">
               <div className="row-1">
-                <div 
-                  className="image-div"
-                  onMouseEnter={() => handleModalRender(4)} 
-                  >
                   <Image
+                    className={elementClass4}
+                    onMouseEnter={() => setElementClass4("portfolio-focus-image-div")} 
+                    onMouseLeave={() => setElementClass4("image-div")} 
+                    onClick={() => handleModalRender(4)} 
                     loader={myLoader}
                     src="1.jpg"
                     alt="Picture of the author"
                     width={175}
                     height={125}
                     />
-                </div>
-                  <div 
-                  className="image-div"
-                  onMouseEnter={() => handleModalRender(5)} 
-                   >
                     <Image
+                    className={elementClass5}
+                    onMouseEnter={() => setElementClass5("portfolio-focus-image-div")} 
+                    onMouseLeave={() => setElementClass5("image-div")} 
+                    onClick={() => handleModalRender(5)} 
                     loader={myLoader}
                     src="2.jpg"
                     alt="Picture of the author"
@@ -129,32 +132,29 @@ export default function Me() {
                     height={125}
                       />
                   </div>
-                  </div>
                   <div className="row-2">
-                    <div 
-                    className="image-div"
-                    onMouseEnter={() => handleModalRender(6)} 
-                      >
                       <Image
-                      loader={myLoader}
-                      src="3.jpg"
-                      alt="Picture of the author"
-                      width={175}
-                      height={125}
+                        className={elementClass6}
+                        onMouseEnter={() => setElementClass6("portfolio-focus-image-div")} 
+                        onMouseLeave={() => setElementClass6("image-div")} 
+                        onClick={() => handleModalRender(6)} 
+                        loader={myLoader}
+                        src="3.jpg"
+                        alt="Picture of the author"
+                        width={175}
+                        height={125}
                       />
-                  </div>
-                  <div 
-                    className="image-div"
-                    onMouseEnter={() => handleModalRender(7)} 
-                    >
                       <Image
-                      loader={myLoader}
-                      src="4.jpg"
-                      alt="Picture of the author"
-                      width={175}
-                      height={125}
+                        className={elementClass7}
+                        onMouseEnter={() => setElementClass7("portfolio-focus-image-div")} 
+                        onMouseLeave={() => setElementClass7("image-div")} 
+                        onClick={() => handleModalRender(7)} 
+                        loader={myLoader}
+                        src="4.jpg"
+                        alt="Picture of the author"
+                        width={175}
+                        height={125}
                       />
-                  </div>
                 </div>
               </div>
               <Modal show={showModal} onHide={() => setShowModal(false)}>
