67249db6d3101e187ebec41dcbea15b8dd81c898
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-12 22:19:26 +0800

    Update downloadPDF
    Increase width of select and remove unused css
    Update Select to only display one CV

--- components/landingPageComponents/downloadPDF.tsx
+++ components/landingPageComponents/downloadPDF.tsx
@@ -1,17 +1,16 @@
-export default function DownloadPDF() {
+const DownloadPDF = () => {
 
-    const requestRegion = (event) => {
-        window.open(event.target.value);
-        if(!window.open(event.target.value)) {
-            window.location.href = event.target.value;
-        }
-     };
+    const requestRegion = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = event.currentTarget.value;
+        window.open(value, '_blank');
+    };
 
     return (
         <select title="View CV" id="resume" onChange={requestRegion}>
-            <option> View CV     </option>
-            <option value="https://robertpethercvbucket.s3.amazonaws.com/Robert+Pether+Full-Stack+Developer+CV+Resume.pdf">Full-Stack JS CV</option>
-            <option value="https://robertpethercvbucket.s3.amazonaws.com/Robert+Pether+Front-End+Developer+CV+Resume.pdf">Front-End React CV</option>
+            <option>View CV</option>
+            <option value="https://robertpethercvbucket.s3.amazonaws.com/Robert+Pether+Full-Stack+Developer+CV+Resume.pdf">Full-Stack .NET</option>
         </select>
     );
 };
+
+export default DownloadPDF;
